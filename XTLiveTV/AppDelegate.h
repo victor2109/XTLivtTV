34f7ff92de40f587d1896db1838e09dc6a5fc771
victor <xiaotong@youku.com>
2017-03-03 09:57:07 +0800

    项目搭建

---
+++ XTLiveTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTLiveTV
+//
+//  Created by victor on 17/3/2.
+//  Copyright © 2017年 victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
